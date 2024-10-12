77ed5f4fd4c27b2dff928b39939b7915ffabcf94
Anthony Salemo <ajssalemo@gmail.com>
2024-10-12 19:06:38 -0400

    able to list specific deployment names via route params

--- controllers/getDeploymentController.go
+++ controllers/getDeploymentController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	config "github.com/Ajsalemo/kubernetes-client-application/config"
 	"github.com/gofiber/fiber/v2"
@@ -17,21 +18,30 @@ func GetDeployments(c *fiber.Ctx) error {
 		zap.L().Error(err.Error())
 		panic(err)
 	}
+	// Check if the parameter is empty - if so, return a 400 for bad request
+	if c.Params("deployment") == "" {
+		return c.Status(400).JSON(fiber.Map{"error": "Deployment name is required"})
+	}
+	zap.L().Info("User provided deployment name: " + c.Params("deployment"))
 
 	deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
 	listOptions := metav1.ListOptions{
-		LabelSelector: "app=nginx-deployment",
+		FieldSelector: fmt.Sprintf("metadata.name=%s", c.Params("deployment")),
 	}
-	getDeployment, err := deploymentsClient.List(context.TODO(), metav1.ListOptions{LabelSelector: listOptions.LabelSelector})
+	getDeployment, err := deploymentsClient.List(context.TODO(), metav1.ListOptions{FieldSelector: listOptions.FieldSelector})
 
 	if err != nil {
 		zap.L().Error(err.Error())
 		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
 	}
-
+	// Log the deployment names
 	for _, d := range getDeployment.Items {
 		zap.L().Info(" * " + d.GetName())
 	}
-
-	return c.JSON(fiber.Map{"deployments": getDeployment.Items})
+	// If the deployment is not found, return a 404
+	if len(getDeployment.Items) == 0 {
+		return c.Status(404).JSON(fiber.Map{"error": "Deployment not found"})
+	} else {
+		return c.JSON(fiber.Map{"deployments": getDeployment.Items})
+	}
 }
